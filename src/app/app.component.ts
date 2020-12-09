9b846da9e257b509adb2f908ce9ab75bd06689ad
Lia Cuello <liacuello@live.com>
2020-12-09 17:47:32 -0400

    *

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,70 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Examen1';
+  guardados = [{}];
+  valor1: number;
+  valor2: number;
+  resultado:number;
+  obteniendo : any;
+
+  constructor(){
+  
+  }
+
+  eliminartodo(){
+    localStorage.clear();
+  }
+  
+  suma(){
+    this.resultado = this.valor1 + this.valor2;
+
+    this.guardados = [{
+      primero : this.valor1,
+      segundo : this.valor2,
+      res : this.resultado,
+    }];
+
+    localStorage.setItem("Suma", JSON.stringify(this.guardados));
+    this.obteniendo = JSON.parse(localStorage.getItem("Suma"));
+    console.log(this.obteniendo);
+  }
+  resta(){
+    this.resultado = this.valor1 - this.valor2;
+
+    this.guardados = [{
+      primero : this.valor1,
+      segundo : this.valor2,
+      res : this.resultado,
+    }];
+
+    localStorage.setItem("Resta", JSON.stringify(this.guardados));
+    this.obteniendo = JSON.parse(localStorage.getItem("Resta"));
+    console.log(this.obteniendo);
+  }
+  multiplicacion(){
+    this.resultado = this.valor1 * this.valor2;
+
+    this.guardados = [{
+      primero : this.valor1,
+      segundo : this.valor2,
+      res : this.resultado,
+    }];
+
+    localStorage.setItem("Multiplicacion", JSON.stringify(this.guardados));
+    this.obteniendo = JSON.parse(localStorage.getItem("Multiplicacion"));
+    console.log(this.obteniendo);
+  }
+  division(){
+    this.resultado = this.valor1 / this.valor2;
+
+    this.guardados = [{
+      primero : this.valor1,
+      segundo : this.valor2,
+      res : this.resultado,
+    }];
+
+    localStorage.setItem("Division", JSON.stringify(this.guardados));
+    this.obteniendo = JSON.parse(localStorage.getItem("Division"));
+    console.log(this.obteniendo);
+  }
 }
